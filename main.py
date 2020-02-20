4cb28a05f5839dd12725fb5e32718f214d5308b7
NoahH <noahhalig@gmsil.com>
2020-02-20 13:28:47 -0800

    Added Functionality

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 #!/usr/bin/env python
 """Implements math functions without using operators except for '+' and '-' """
 
-__author__ = "???"
+__author__ = "Noah"
 
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
+    return (x + y)
 
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    product = 0
+    if(y >= 0):
+        for i in range(0, y):
+            product = add(product, x)
+        return product
+    else:
+        for i in range(y, 0):
+            product = add(product, x)
+        return -product    
+    
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    powerProduct = 1
+    for i in range(0, n):
+        powerProduct = multiply(powerProduct, x)
+    return powerProduct
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    facValue = 1
+    for i in range(1, x + 1):
+        facValue = multiply(facValue, i)
+    return facValue
 
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    fibList = [0, 1]
+    i = 2
+    while(len(fibList) < n):
+        fibList.append(add(fibList[i-1], fibList[i-2]))
+        i = add(i, 1)
+    return fibList[n - 1]
 
 
 if __name__ == '__main__':
-    # your code to call functions above
-    pass
+    print(add(2, 4))
+    print(multiply(6, -8))
+    print(power(2, 8))
+    print(factorial(4))
+    print(fibonacci(8))
